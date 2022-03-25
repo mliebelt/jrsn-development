a2b5b2bcae0e3d86061f3345edc96a64b2e7084a
Markus Liebelt <mliebelt@hey.com>
2022-03-25 11:40:45 +0100

    Erneuter Versuch, node_modules zu löschen

--- node_modules/mousetrap-ts/config/helpers.ts
+++
@@ -1,7 +0,0 @@
-import { join, resolve } from 'path';
-
-const ROOT = resolve(__dirname, '..');
-export const root = (path: string): string => join(ROOT, path);
-
-export const exclude = /(node_modules|bower_components|dist|public|build|.tmp|temp)(\/|\\)/;
-
